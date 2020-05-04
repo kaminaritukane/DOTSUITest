72e8ac36cadb5b713e4bc33ee73c8934424e3a47
kaminaritukane <kaminaritukane@163.com>
2020-05-04 18:13:25 +0800

    First commit with build bug fixed

---
+++ Assets/com.dotsui.core/DotsUI.Layout/VerticalLayout.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace DotsUI.Layout
+{
+#pragma warning disable 0649
+    struct LayoutGroup : IComponentData
+    {
+        public float Left;
+        public float Right;
+        public float Top;
+        public float Bottom;
+    }
+
+    struct VerticalLayout : IComponentData
+    {
+        public float Spacing;
+        public bool ControlWidth;
+        public bool ControlHeight;
+    }
+
+    struct HorizontalLayout : IComponentData
+    {
+        public float Spacing;
+        public bool ControlWidth;
+        public bool ControlHeight;
+    }
+
+    struct GridLayout : IComponentData
+    {
+        public float2 Spacing;
+    }
+#pragma warning restore 0649
+}
